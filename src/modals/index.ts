874951f07b43b511348938ca67437d6d0ada59d6
Dimitriy <43708417+ay0ks@users.noreply.github.com>
2024-04-13 18:29:40 +0200

    add: index.ts

---
+++ src/modals/index.ts
@@ -0,0 +1,13 @@
+import { SampleModal } from "./debug";
+import { DevicesModal } from "./devices";
+import { LoginModal } from "./login";
+import { VaultsModal } from "./vaults";
+import { WarningModal } from "./warning";
+
+export {
+    SampleModal,
+    DevicesModal,
+    LoginModal,
+    VaultsModal,
+    WarningModal
+};
